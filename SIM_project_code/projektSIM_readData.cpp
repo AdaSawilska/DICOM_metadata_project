82b1bb71ff2f2d7bd43036c87da05367fd9f7d76
AdaSawilska <01143808@pw.edu.pl>
2024-04-27 11:50:25 +0200

    all dicom metadata to odbc, convert dicom to jpg draft

--- SIM_project_code/projektSIM_readData.cpp
+++ SIM_project_code/projektSIM_readData.cpp
@@ -52,6 +52,34 @@ SQLRETURN exist_check(string table, string column, string id_to_check, SQLHDBC h
     return retcode;
 }
 
+
+string get_value(string wanted_column, string table, string given_column, string value, SQLHDBC hdbc) {
+    SQLHSTMT hstmt;
+    SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt); // Allocate statement handle
+    SQLINTEGER id;
+    SQLLEN indicator;
+
+    string query = "SELECT " + wanted_column + " FROM " + table + " WHERE " + given_column + " = '" + value + "';";
+    if (SQLExecDirect(hstmt, (SQLCHAR *)query.c_str(), SQL_NTS) != SQL_SUCCESS) {
+        cerr << "Error executing SQL statement" << endl;
+        SQLFreeHandle(SQL_HANDLE_STMT, hstmt);
+        return ""; // Return empty string on error
+    }
+
+    // Process the result
+    while (SQLFetch(hstmt) == SQL_SUCCESS) {
+        SQLGetData(hstmt, 1, SQL_C_SLONG, &id, sizeof(id), &indicator);
+        if (indicator != SQL_NULL_DATA) {
+            return to_string(id);
+        } else {
+            cerr << "No data found" << endl;
+            return ""; // Return empty string if no data found
+        }
+    }
+    return "";
+}
+
+
 void tree(string p, SQLHDBC hdbc) {
     DIR * dirp = opendir(p.c_str()); //otwarcie katalogu ze sciezki
     dirent * dp; //uchwyt do struktury danych
@@ -76,6 +104,9 @@ void tree(string p, SQLHDBC hdbc) {
 
             // odczytanie danych z pliku .dcm
             dicomhero::DataSet loadedDataSet(dicomhero::CodecFactory::load(pp, 2048));
+            dicomhero::Image image(loadedDataSet.getImageApplyModalityTransform(0));
+            string jpg = p + "/obrazek";
+            dicomhero::CodecFactory::save(loadedDataSet, jpg, dicomhero::codecType_t::jpeg);
             // Patient ID
             string patientID = loadedDataSet.getString(dicomhero::TagId(dicomhero::tagId_t::PatientID_0010_0020), 0);
             cout << patientID << endl;
@@ -126,36 +157,75 @@ void tree(string p, SQLHDBC hdbc) {
 
             //sprawdzenie czy w bazie jest juz taki Patient z takim samym dicomid:
             retcode = exist_check("Patient", "dicomid", patientID, hdbc);
-
-
             if(retcode != SQL_NO_DATA) {
                 cout<<"Pacjent o takim id juz istnieje w bazie"<<endl;
             }
-            //jesli nie ma takiego badania to wprowadzenie go do bazy:
+            //jesli nie ma takiego pacjenta to wprowadzenie go do bazy:
             else {
                 //dodanie nowego pacjenta
                 cout << "Wprowadzam nowego pacjenta do bazy..." << endl;
-                retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-                // CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
-                
+                retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);                
                 string komenda1 = "INSERT INTO Patient(birthdate, sex, name, dicomid) VALUES ('"+formattedBirthDate+"','"+patientSex+"','"+patientNameCharacter_S+"', '"+patientID+"')";
                 cout << komenda1 << endl;
                 retcode = SQLExecDirect(hstmt,(SQLCHAR*) komenda1.c_str(), SQL_NTS);
-                // CHECK_ERROR(retcode, "SQLExecDirect() INSERT", hstmt, SQL_HANDLE_STMT);
                 retcode = SQLFreeStmt(hstmt, SQL_DROP);
-                
-                // retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
-                // CHECK_ERROR(retcode, "SQLAllocHandle(SQL_HANDLE_STMT)", hstmt, SQL_HANDLE_STMT);
-                // string findID = "SELECT max(id) FROM pacjent";
-                // retcode = SQLExecDirect(hstmt,(SQLCHAR*) findID.c_str(), SQL_NTS);
-                // CHECK_ERROR(retcode, "SQLExecDirect() SELECT", hstmt, SQL_HANDLE_STMT);
-                // retcode = SQLFetch(hstmt);
-                // CHECK_ERROR(retcode, "SQLFetch()", hstmt, SQL_HANDLE_STMT);
-                // retcode = SQLGetData(hstmt, 1, SQL_C_DEFAULT, &cMAXID, 0,  &lenMAXID);
-                // CHECK_ERROR(retcode, "SQLGetData()", hstmt, SQL_HANDLE_STMT);
-                // SQLFreeStmt(hstmt, SQL_DROP);
-                // int pacjentIDint = cMAXID;
-                // cout << pacjentIDint<< endl;
+            }
+
+            //sprawdzenie czy w bazie jest juz takie Study z takim samym StudyUID:
+            retcode = exist_check("Study", "studyuid", studyInstanceUID, hdbc);
+            if(retcode != SQL_NO_DATA) {
+                cout<<"Study o takim uid juz istnieje w bazie"<<endl;
+            }
+            //jesli nie ma takiego Study to wprowadzenie go do bazy:
+            else {
+                //dodanie nowego Study
+                cout << "Wprowadzam nowe study do bazy..." << endl;
+                retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+                // sprawdzenie ID pacjenta w odbc dla danego study
+                string patient_database_id = get_value("id", "Patient", "dicomid", patientID, hdbc);
+                cout << "ID pacjenta w ODBC: " << patient_database_id << endl;
+                string komenda1 = "INSERT INTO Study(studyuid, patientid, studydate, studytime, studydescription) VALUES ('"+studyInstanceUID+"','"+patient_database_id+"','"+formattedStudyDate+"', '"+formattedStudyTime+"', '"+studyDescription+"')";
+                cout << komenda1 << endl;
+                retcode = SQLExecDirect(hstmt,(SQLCHAR*) komenda1.c_str(), SQL_NTS);
+                retcode = SQLFreeStmt(hstmt, SQL_DROP);
+            }
+
+            //sprawdzenie czy w bazie jest juz takie Series z takim samym SeriesUID:
+            retcode = exist_check("Series", "seriesuid", seriesInstanceUID, hdbc);
+            if(retcode != SQL_NO_DATA) {
+                cout<<"Series o takim uid juz istnieje w bazie"<<endl;
+            }
+            //jesli nie ma takiego Series to wprowadzenie go do bazy:
+            else {
+                //dodanie nowego Series
+                cout << "Wprowadzam nowe study do bazy..." << endl;
+                retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+                // sprawdzenie StudyUID w odbc dla danego study
+                string study_database_id = get_value("id", "Study", "studyuid", studyInstanceUID, hdbc);
+                cout << "StudyID w ODBC: " << study_database_id << endl;
+                string komenda1 = "INSERT INTO Series(seriesuid, studyid, modality, bodypart, seriesdescription) VALUES ('"+seriesInstanceUID+"','"+study_database_id+"','"+modality+"', '"+bodyPartExamined+"', '"+seriesDescription+"')";
+                cout << komenda1 << endl;
+                retcode = SQLExecDirect(hstmt,(SQLCHAR*) komenda1.c_str(), SQL_NTS);
+                retcode = SQLFreeStmt(hstmt, SQL_DROP);
+            }
+
+            //sprawdzenie czy w bazie jest juz taki Image z taka sciezka:
+            retcode = exist_check("Image", "imagepath", pp, hdbc);
+            if(retcode != SQL_NO_DATA) {
+                cout<<"Image juz istnieje w bazie"<<endl;
+            }
+            //jesli nie ma takiego Series to wprowadzenie go do bazy:
+            else {
+                //dodanie nowego Series
+                cout << "Wprowadzam nowe image do bazy..." << endl;
+                retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);
+                // sprawdzenie SeriesUID w odbc dla danego image
+                string series_database_id = get_value("id", "Series", "seriesuid", seriesInstanceUID, hdbc);
+                cout << "SeriesID w ODBC: " << series_database_id << endl;
+                string komenda1 = "INSERT INTO Image(imagepath, seriesid) VALUES ('"+pp+"','"+series_database_id+"')";
+                cout << komenda1 << endl;
+                retcode = SQLExecDirect(hstmt,(SQLCHAR*) komenda1.c_str(), SQL_NTS);
+                retcode = SQLFreeStmt(hstmt, SQL_DROP);
             }
 
 
